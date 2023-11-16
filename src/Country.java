09c3666f33e6c5859f417ae399a3fe636de5aea2
jboli <jboli@John>
2023-11-15 22:01:24 -0800

    test

--- src/Country.java
+++ src/Country.java
@@ -9,8 +9,8 @@ public class Country {
 	private Hashtable<Double, Integer> tempyear = new Hashtable();
 	
 	
-	public Country(String name2, Hashtable<Integer, Double> yeartemp2, Hashtable<Double, Integer> tempyear2 ) {
-		this.name=name2;
+	public Country(String name3, Hashtable<Integer, Double> yeartemp2, Hashtable<Double, Integer> tempyear2 ) {
+		this.name=name3;
 		this.yeartemp = yeartemp2;
 		this.tempyear = tempyear2;
 	}
