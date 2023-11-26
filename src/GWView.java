9bcce876404d8e3a0fcf522d00245b6d0432c59b
jboli <jboli@john>
2023-11-25 22:40:36 -0800

    Working on GUI

--- src/GWView.java
+++ src/GWView.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
@@ -13,6 +15,8 @@ import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
 import javax.swing.border.Border;
 
 
@@ -23,6 +27,7 @@ public class GWView extends JFrame{
 	private JPanel rightPanel;
 	private JPanel mainPanel;
 	private JComboBox comboBox;
+	GridBagConstraints gbc = new GridBagConstraints();
 
 	public GWView() {
 	
@@ -45,30 +50,32 @@ public class GWView extends JFrame{
 		add(centerPanel, BorderLayout.CENTER);
 		add(rightPanel, BorderLayout.EAST);
 		
+		
 		setVisible(true);
 	
 	}
 	
 	private void createleftPanel() {
 		
-		
-		Library june = new Library();
-		int n = june.indexlist.size();
-		String[] list = new String[n];
-		for (int i = 0; i<n; i++) {
-			list[i] = june.indexlist.get(i);
-			System.out.println(june.getpercentChange("Philippines", 1961, 2022));
-//			System.out.println(june.getcoldestTemp("Philippines"));
-		}
+		//create top panel
+		JPanel panel1 = new JPanel();
 		
 		
-		JLabel title = new JLabel("TEMPERATURE DIFFERENCE CALCULATOR");
-		title.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		//create center panel
+		JPanel panel2 = new JPanel();
+		panel2.setLayout(new GridBagLayout());
+		
+		//create bottom panel
+		JPanel panel3 = new JPanel();
+		
+		
+		
+		//PANEL 1
+		JLabel title = new JLabel("TEMPERATURE DIFFERENCE CALCULATOR BY YEAR");
+		title.setFont(new Font("Times New Roman", Font.BOLD, 15));
 		title.setAlignmentX(CENTER_ALIGNMENT);
-		JLabel title2 = new JLabel("BY YEAR");
-		title2.setFont(new Font("Times New Roman", Font.BOLD, 20));
-		title2.setAlignmentX(CENTER_ALIGNMENT);
 		
+		//PANEL 2
 		JLabel instruction = new JLabel("Pick a Country:");
 		instruction.setAlignmentX(CENTER_ALIGNMENT);
 		JLabel year1 = new JLabel("Year 1");
@@ -76,26 +83,58 @@ public class GWView extends JFrame{
 		JLabel year2 = new JLabel("Year 2");
 		year2.setAlignmentX(CENTER_ALIGNMENT);
 		
-		BoxLayout layout = new BoxLayout(leftPanel, BoxLayout.Y_AXIS);
-		leftPanel.setLayout(layout);
-		JPanel inpanel = new JPanel();
-		inpanel.setLayout(new BoxLayout(inpanel, BoxLayout.PAGE_AXIS));
-
+		JTextField field1 = new JTextField();
+		field1.setPreferredSize(new Dimension (100, 30));
+		JTextField field2 = new JTextField();
+		field2.setPreferredSize(new Dimension (100,30));
+		
+//		BoxLayout layout = new BoxLayout(leftPanel, BoxLayout.Y_AXIS);
+//		leftPanel.setLayout(layout);
+		
+		//get string array of all countries
+		//using library object
+		Library june = new Library();
+		int n = june.indexlist.size();
+		String[] list = new String[n];
+		for (int i = 0; i<n; i++) {
+			list[i] = june.indexlist.get(i);
+		}
 		comboBox = new JComboBox(list);
-		comboBox.setMaximumSize(comboBox.getPreferredSize());
+		
+		//PANEL 3
+		/**
+		 
+		 **/
 		
 		
-		inpanel.add(comboBox);
+		panel1.add(title, BorderLayout.CENTER);
 		
-		leftPanel.add(title, BorderLayout.CENTER);
-		leftPanel.add(title2, BorderLayout.CENTER);
-		leftPanel.add(Box.createVerticalStrut(20));
-		leftPanel.add(instruction,BorderLayout.CENTER);
-		leftPanel.add(inpanel);
-		leftPanel.add(Box.createVerticalStrut(20));
-		leftPanel.add(year1);
-		leftPanel.add(Box.createVerticalStrut(20));
-		leftPanel.add(year2);
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		panel2.add(instruction, gbc);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		panel2.add(comboBox, gbc);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 2;
+		panel2.add(year1, gbc);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 3;
+		panel2.add(field1, gbc);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 4;
+		panel2.add(year2, gbc);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 5;
+		panel2.add(field2, gbc);
+		
+		leftPanel.add(panel1);
+		leftPanel.add(panel2);
 		
 	}
 	
