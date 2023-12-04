e6af3ab8351583d92d8a352bebcff727231fcfee
jboli <jboli@John>
2023-12-03 23:15:37 -0800

    All 3 panels finished.

--- src/GWView.java
+++ src/GWView.java
@@ -35,13 +35,18 @@ public class GWView extends JFrame{
 	private JComboBox comboBox;
 	private ImageIcon image1;
 	private Double tempdiff = 0.0;
+	private Integer yearhot = 0000;
+	private Integer yearcold = 0000;
+	private Double hottesttemp = 0.0;
+	private Double coldesttemp = 0.0;
+	private Double ratediff;
 	
 
 
 	public GWView() {
 		
 		super("Global Warming");
-		setSize(1000, 340);
+		setSize(1450, 340);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		leftPanel = new JPanel();
@@ -52,12 +57,16 @@ public class GWView extends JFrame{
 		
 		
 		createleftPanel();
+		createmiddlePanel();
+		createrightPanel();
 		
 		Border blackline = BorderFactory.createLineBorder(Color.black);
 		add(leftPanel, BorderLayout.WEST);
 		leftPanel.setBorder(blackline);
 		add(centerPanel, BorderLayout.CENTER);
+		centerPanel.setBorder(blackline);
 		add(rightPanel, BorderLayout.EAST);
+		
 		
 		setVisible(true);
 	
@@ -112,11 +121,13 @@ public class GWView extends JFrame{
 		
 		//PANEL 4 - for radio buttons
 		JRadioButton radio1 = new JRadioButton();
-		JLabel celsius = new JLabel("Convert to Celsius");
+		JLabel celsius = new JLabel("Show in Celsius");
 		JRadioButton radio2 = new JRadioButton();
-		JLabel fahrenheit = new JLabel("Convert to Fahrenheit");
+		JLabel fahrenheit = new JLabel("Show in Fahrenheit");
 		
 		panel1.add(title);
+		
+		panel1.add(Box.createRigidArea(new Dimension(0,10)));
 		
 		panel1.add(instruction);
 		
@@ -132,13 +143,15 @@ public class GWView extends JFrame{
 		
 		panel1.add(field2);
 		
-		panel1.add(panel2);
+		
 		
 		panel1.add(panel3);
 		
 		panel1.add(panel4);
 		
 		panel2.add(submit);
+		
+		panel1.add(panel2);
 		
 		panel3.add(difference);
 		panel3.add(differencenumber);
@@ -153,6 +166,116 @@ public class GWView extends JFrame{
 	
 	private void createmiddlePanel() {
 		
+		Library june = new Library();
+		int n = june.indexlist.size();
+		String[] list = new String[n];
+		for (int i = 0; i<n; i++) {
+			list[i] = june.indexlist.get(i);
+		}
+		comboBox = new JComboBox(list);
+		
+		
+		JLabel title = new JLabel("HOTTEST AND COLDEST YEAR BY COUNTRY OR TERRITORY");
+		title.setFont(new Font("Times New Roman", Font.BOLD, 15));
+		title.setAlignmentX(CENTER_ALIGNMENT);
+		JLabel instruction = new JLabel("Pick a Country or Territory:");
+		instruction.setAlignmentX(CENTER_ALIGNMENT);
+		JLabel hottemp = new JLabel("Hottest Temperature: " + String.valueOf(hottesttemp));
+		hottemp.setAlignmentX(CENTER_ALIGNMENT);
+		JLabel year = new JLabel("in the year of "+String.valueOf(yearhot));
+		year.setAlignmentX(CENTER_ALIGNMENT);
+		JLabel coldtemp = new JLabel("Coldest Temperature: " + String.valueOf(coldesttemp));
+		coldtemp.setAlignmentX(CENTER_ALIGNMENT);
+		JLabel year1 = new JLabel(String.valueOf("in the year of "+ String.valueOf(yearcold)));
+		year1.setAlignmentX(CENTER_ALIGNMENT);
+		JButton submit = new JButton("SUBMIT");
+		submit.setAlignmentX(CENTER_ALIGNMENT);
+		JRadioButton radio1 = new JRadioButton();
+		JLabel celsius = new JLabel("Show in Celsius");
+		JRadioButton radio2 = new JRadioButton();
+		JLabel fahrenheit = new JLabel("Show in Fahrenheit");
+		
+		JPanel panel1 = new JPanel();
+		panel1.setLayout(new BoxLayout(panel1, BoxLayout.Y_AXIS));
+		
+		JPanel panel2 = new JPanel();
+		JPanel panel3 = new JPanel();
+		
+		panel1.add(title);	
+		panel1.add(Box.createRigidArea(new Dimension(0,10)));
+		panel1.add(instruction);
+		panel1.add(comboBox);
+		
+		panel2.add(radio1);
+		panel2.add(celsius);
+		panel3.add(radio2);
+		panel3.add(fahrenheit);
+		panel1.add(panel2);
+		panel1.add(panel3);
+		panel1.add(Box.createRigidArea(new Dimension(0,10)));
+		panel1.add(hottemp);
+		panel1.add(year);
+		panel1.add(coldtemp);
+		panel1.add(year1);
+		panel1.add(Box.createRigidArea(new Dimension(0,15)));
+		panel1.add(submit);
+		
+		centerPanel.add(panel1);
+
+	}
+	
+	private void createrightPanel() {
+		
+		Library june = new Library();
+		int n = june.indexlist.size();
+		String[] list = new String[n];
+		for (int i = 0; i<n; i++) {
+			list[i] = june.indexlist.get(i);
+		}
+		comboBox = new JComboBox(list);
+		
+		JLabel title = new JLabel("TEMPERATURE RATE CALCULATOR BY COUNTRY OR TERRITORY");
+		title.setFont(new Font("Times New Roman", Font.BOLD, 15));
+		title.setAlignmentX(CENTER_ALIGNMENT);
+		JLabel instruction = new JLabel("Pick a Country or Territory:");
+		instruction.setAlignmentX(CENTER_ALIGNMENT);
+		JLabel instruction2 = new JLabel("Enter years between 1961 and 2022");
+		instruction2.setAlignmentX(CENTER_ALIGNMENT);
+		JLabel instruction3 = new JLabel("This will calculate the temperature rate increase or decrease between both years.");
+		instruction3.setAlignmentX(CENTER_ALIGNMENT);
+		JLabel year1 = new JLabel("Year 1");
+		year1.setAlignmentX(CENTER_ALIGNMENT);
+		JLabel year2 = new JLabel("Year 2");
+		year2.setAlignmentX(CENTER_ALIGNMENT);
+		JTextField field1 = new JTextField();
+		field1.setPreferredSize(new Dimension (70, 30));
+		JTextField field2 = new JTextField();
+		field2.setPreferredSize(new Dimension (70,30));
+		JLabel rate = new JLabel("The rate increase or decrease is " + String.valueOf(ratediff) + "%" );
+		rate.setAlignmentX(CENTER_ALIGNMENT);
+		JButton submit = new JButton("SUBMIT");
+		submit.setAlignmentX(CENTER_ALIGNMENT);
+		
+		JPanel panel1 = new JPanel();
+		panel1.setLayout(new BoxLayout(panel1, BoxLayout.Y_AXIS));
+		
+		panel1.add(title);
+		panel1.add(Box.createRigidArea(new Dimension(0,10)));
+		panel1.add(instruction);
+		panel1.add(comboBox);
+		panel1.add(instruction2);
+		panel1.add(instruction3);
+		panel1.add(year1);
+		panel1.add(field1);
+		panel1.add(year2);
+		panel1.add(field2);
+		
+		panel1.add(Box.createRigidArea(new Dimension(10,10)));
+		panel1.add(rate);
+		panel1.add(Box.createRigidArea(new Dimension(10,10)));
+		panel1.add(submit);
+		
+		rightPanel.add(panel1);
 	}
 	
 	public static void main(String[] args)
