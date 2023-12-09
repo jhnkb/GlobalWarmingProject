244e6e7626a99a59418c6f164e924f4c19b9e897
jboli <jboli@john>
2023-12-08 23:08:33 -0800

    readded middle panel and right panel code

--- src/GWView.java
+++ src/GWView.java
@@ -273,6 +273,121 @@ public class GWView extends JFrame {
 		leftPanel.add(panel1);
 		
 	}
+//	private void createmiddlePanel() {
+//	
+//	Library june = new Library();
+//	int n = june.indexlist.size();
+//	String[] list = new String[n];
+//	for (int i = 0; i<n; i++) {
+//		list[i] = june.indexlist.get(i);
+//	}
+//	comboBox = new JComboBox(list);
+//	
+//	
+//	JLabel title = new JLabel("HOTTEST AND COLDEST YEAR BY COUNTRY OR TERRITORY");
+//	title.setFont(new Font("Times New Roman", Font.BOLD, 15));
+//	title.setAlignmentX(CENTER_ALIGNMENT);
+//	JLabel instruction = new JLabel("Pick a Country or Territory:");
+//	instruction.setAlignmentX(CENTER_ALIGNMENT);
+//	JLabel hottemp = new JLabel("Hottest Temperature: " + String.valueOf(hottesttemp));
+//	hottemp.setAlignmentX(CENTER_ALIGNMENT);
+//	JLabel year = new JLabel("in the year of "+String.valueOf(yearhot));
+//	year.setAlignmentX(CENTER_ALIGNMENT);
+//	JLabel coldtemp = new JLabel("Coldest Temperature: " + String.valueOf(coldesttemp));
+//	coldtemp.setAlignmentX(CENTER_ALIGNMENT);
+//	JLabel year1 = new JLabel(String.valueOf("in the year of "+ String.valueOf(yearcold)));
+//	year1.setAlignmentX(CENTER_ALIGNMENT);
+//	JButton submit = new JButton("SUBMIT");
+//	submit.setAlignmentX(CENTER_ALIGNMENT);
+//	JRadioButton radio1 = new JRadioButton();
+//	JLabel celsius = new JLabel("Show in Celsius");
+//	JRadioButton radio2 = new JRadioButton();
+//	JLabel fahrenheit = new JLabel("Show in Fahrenheit");
+//	
+//	JPanel panel1 = new JPanel();
+//	panel1.setLayout(new BoxLayout(panel1, BoxLayout.Y_AXIS));
+//	
+//	JPanel panel2 = new JPanel();
+//	JPanel panel3 = new JPanel();
+//	
+//	panel1.add(title);	
+//	panel1.add(Box.createRigidArea(new Dimension(0,10)));
+//	panel1.add(instruction);
+//	panel1.add(comboBox);
+//	
+//	panel2.add(radio1);
+//	panel2.add(celsius);
+//	panel3.add(radio2);
+//	panel3.add(fahrenheit);
+//	panel1.add(panel2);
+//	panel1.add(panel3);
+//	panel1.add(Box.createRigidArea(new Dimension(0,10)));
+//	panel1.add(hottemp);
+//	panel1.add(year);
+//	panel1.add(coldtemp);
+//	panel1.add(year1);
+//	panel1.add(Box.createRigidArea(new Dimension(0,15)));
+//	panel1.add(submit);
+//	
+//	centerPanel.add(panel1);
+//
+//}
+//
+//private void createrightPanel() {
+//	
+//	Library june = new Library();
+//	int n = june.indexlist.size();
+//	String[] list = new String[n];
+//	for (int i = 0; i<n; i++) {
+//		list[i] = june.indexlist.get(i);
+//	}
+//	comboBox2 = new JComboBox(list);
+//	
+//	JLabel title = new JLabel("TEMPERATURE RATE CALCULATOR BY COUNTRY OR TERRITORY");
+//	title.setFont(new Font("Times New Roman", Font.BOLD, 15));
+//	title.setAlignmentX(CENTER_ALIGNMENT);
+//	JLabel instruction = new JLabel("Pick a Country or Territory:");
+//	instruction.setAlignmentX(CENTER_ALIGNMENT);
+//	JLabel instruction2 = new JLabel("Enter years between 1961 and 2022");
+//	instruction2.setAlignmentX(CENTER_ALIGNMENT);
+//	JLabel instruction3 = new JLabel("(This will calculate the temperature rate increase or decrease between both years.)");
+//	instruction3.setAlignmentX(CENTER_ALIGNMENT);
+//	JLabel year1 = new JLabel("Year 1");
+//	year1.setAlignmentX(CENTER_ALIGNMENT);
+//	JLabel year2 = new JLabel("Year 2");
+//	year2.setAlignmentX(CENTER_ALIGNMENT);
+//	JTextField field1 = new JTextField();
+//	field1.setPreferredSize(new Dimension (70, 30));
+//	JTextField field2 = new JTextField();
+//	field2.setPreferredSize(new Dimension (70,30));
+//	JLabel rate = new JLabel("The rate increase or decrease is " + String.valueOf(ratediff) + "%" );
+//	rate.setAlignmentX(CENTER_ALIGNMENT);
+//	JButton submit = new JButton("SUBMIT");
+//	submit.setAlignmentX(CENTER_ALIGNMENT);
+//	
+//	JPanel panel1 = new JPanel();
+//	panel1.setLayout(new BoxLayout(panel1, BoxLayout.Y_AXIS));
+//	
+//	panel1.add(title);
+//	panel1.add(Box.createRigidArea(new Dimension(0,10)));
+//	panel1.add(instruction);
+//	panel1.add(comboBox2);
+//	panel1.add(instruction2);
+//	panel1.add(instruction3);
+//	panel1.add(year1);
+//	panel1.add(field1);
+//	panel1.add(year2);
+//	panel1.add(field2);
+//	
+//	panel1.add(Box.createRigidArea(new Dimension(10,10)));
+//	panel1.add(rate);
+//	panel1.add(Box.createRigidArea(new Dimension(10,10)));
+//	panel1.add(submit);
+//	
+//	
+//	
+//	rightPanel.add(panel1);
+//}
 	
 	private void updateSubmitButtonState() {
 		
